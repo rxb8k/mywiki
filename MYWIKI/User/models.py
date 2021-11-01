a84bb874099e4b6e5254e1c9032248c9b602665d
rxb8k <12183887@inha.edu>
2021-11-01 10:50:03 +0900

    User 백 기능 구현 완료

--- MYWIKI/User/models.py
+++ MYWIKI/User/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, username, password):
+    
+        user = self.model(
+                username = username,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+        
+        
+
+    def create_superuser(self, username, password):
+        user = self.create_user(
+            username = username,
+                password=password,
+        )
+        user.set_password(password)
+        user.is_admin = True
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=50, unique=True, default='')
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    objects = UserManager()
+
+    USERNAME_FIELD = "username"   
+
+    def __str__(self):
+        return self.username
