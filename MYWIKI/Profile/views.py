2af3819df502c73b7ca7097db40864c8b02e6979
dy <ekdudd119@naver.com>
2021-11-03 05:58:16 +0900

    profile 프론트 수정-다영

--- MYWIKI/Profile/views.py
+++ MYWIKI/Profile/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Row
 
-# Create your views here.
+def profile_create(request):
+  new_profile=Profile()
+  new_profile.subject=request.POST.get('subject')
+  new_profile.content=request.POST.get('content')
+  return redirect('Profile:detail')
