9411f7a4fbab87e3341d055db368c8ea1722725c
rxb8k <12183887@inha.edu>
2021-11-06 04:21:00 +0900

    이전까지 커밋 사항: 프론트 행 추가 수정, csrf 토큰 처리 추가

--- MYWIKI/Profile/views.py
+++ MYWIKI/Profile/views.py
@@ -2,23 +2,17 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import *
 # Create your views here.
 
-def home(request):
-    return render(request, 'home.html')
-
-def read(request,id):
+def read(request,username):
     profile = get_object_or_404(Profile, pk = id)
     rows = Row.objects.filter(profile=id)
     topics = Topic.objects.filter(profile=id)
     keywords = Keyword.objects.filter(profile=id)
 
     # 키워드 시각화 ?
-    return render(request, 'read.html', {'profile': profile, 'rows':rows, 'topics': topics, 'keywords': keywords})
-
-def view_history(request, id):
-    profile = get_object_or_404(Profile, pk = id)
-    histories = History.objects.filter(profile=id)
-
-    return render(request, 'history.html', {'profile': profile, 'histories': histories})
+    return render(request, 'read.html', 
+    {'profile': profile, 
+    'rows':rows,
+    'topics': topics})
 
 
 def update(req, id):
@@ -29,3 +23,10 @@ def update(req, id):
         profile_obejct=req.getParameterValues("")
         return redirect('/update'+str(Profile.username))
     return render(req, 'update.html', {'data':profile_object})
+
+
+def view_history(request, id):
+    profile = get_object_or_404(Profile, pk = id)
+    histories = History.objects.filter(profile=id)
+
+    return render(request, 'history.html', {'profile': profile, 'histories': histories})
