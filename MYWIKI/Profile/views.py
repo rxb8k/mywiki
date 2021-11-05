59b9169dac0d2e3228ab9e915e2ef18b06de87c9
changhyun <ckdgus7706@naver.com>
2021-11-06 01:07:50 +0900

    fk 수정

--- MYWIKI/Profile/views.py
+++ MYWIKI/Profile/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import *
 # Create your views here.
 
@@ -7,25 +7,25 @@ def home(request):
 
 def read(request,id):
     profile = get_object_or_404(Profile, pk = id)
-    rows = Row.objects.filter(number=id)
-    topics = Topic.objects.filter(number=id)
-    keywords = Keyword.objects.filter(number=id)
+    rows = Row.objects.filter(profile=id)
+    topics = Topic.objects.filter(profile=id)
+    keywords = Keyword.objects.filter(profile=id)
 
     # 키워드 시각화 ?
     return render(request, 'read.html', {'profile': profile, 'rows':rows, 'topics': topics, 'keywords': keywords})
 
 def view_history(request, id):
     profile = get_object_or_404(Profile, pk = id)
-    histories = History.objects.filter(number=id)
+    histories = History.objects.filter(profile=id)
 
     return render(request, 'history.html', {'profile': profile, 'histories': histories})
 
 
-def update(req):
-    profile_obejct=get_object_or_404(Profile, pk=id)
+def update(req, id):
+    profile_object=get_object_or_404(Profile, pk=id)
     if req.method=='POST':
         # profile_object 각각 변수 설정
         # marketPost_object.title=req.POST['title']
         profile_obejct=req.getParameterValues("")
         return redirect('/update'+str(Profile.username))
-    return render(req, 'update.html', {'data':profile_object}
+    return render(req, 'update.html', {'data':profile_object})
